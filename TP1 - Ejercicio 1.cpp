bc942b32a7cb5256b720ee2389af0d805df355fd
Jesica Fernandez <je.fernandez.cai@gmail.com>
2014-04-25 20:04:23 -0300

    Update TP1 - Ejercicio 1.cpp
    Saque variables innecesarias

--- TP1 - Ejercicio 1.cpp
+++ TP1 - Ejercicio 1.cpp
@@ -5,15 +5,14 @@ long int factorial(long int);
 
 void main()
 {
-   long int m,res;
-	printf("Ingrese numero entero (0<=n<=12):");		// long int tiene capacidad para guardar hasta el resultado de 12! (unsigned long int, aunque me duplica la capacidad, tambien)
+   long int m;
+   printf("Ingrese numero entero (0<=n<=12):");	// long int tiene capacidad para guardar hasta el resultado de 12!
    scanf("%ld",&m);
-   res=factorial(m);
-   printf("\nEl factorial de ese numero es: %ld",res);
+   printf("\nEl factorial de ese numero es: %ld",factorial(m));
 	getch();
 }
 
-long int factorial(long int n)		// Si n=0 me devuelve 1 porque sale del for
+long int factorial(long int n)	// Si n=0 me devuelve 1 porque sale del for
 {
    long int j,fac=1;
    for(j=1;j<=n;j++)
